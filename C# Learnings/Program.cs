5d863a2baa97cee123222c933a9bb5d8b96d8ee1
Hamid Yousaf <hamidyousafofficial@gmail.com>
2025-01-08 22:14:06 +0500

    Learned Primary Constructor in C#12.

--- C# Learnings/Program.cs
+++ C# Learnings/Program.cs
@@ -1,23 +1,42 @@
-﻿
-#region C# 12
+﻿#region C# 12
 // collection expression.
 // Collection expression is a concise syntax for creating and initializing the arrays, lists, and other collection types. It replaces the curly braces into square braces.
-int[] array1 = new int[] {1, 2, 3};
-int[] array2 = {1, 2, 3};
+
+int[] array1 = new int[] { 1, 2, 3 };
+int[] array2 = { 1, 2, 3 };
 int[] array3 = [1, 2, 3];
 
-List<int> list1 = new List<int>() { 11, 12, 13};
-List<int> list2 = new () { 11, 12, 13 };
-List<int> list3 = [11, 12, 13 ];
+List<int> list1 = new List<int>() { 11, 12, 13 };
+List<int> list2 = new() { 11, 12, 13 };
+List<int> list3 = [11, 12, 13];
 
 // we can merge arrays and collection type by using spread operator.
 int[] merge = [.. array1];
-int[] merge1 = [.. array1, ..list1];
-int[] merge2 = [.. array1, 6, 7, 8, ..list1];
+int[] merge1 = [.. array1, .. list1];
+int[] merge2 = [.. array1, 6, 7, 8, .. list1];
 
 for (int i = 0; i < merge2.Length; i++)
 {
     Console.WriteLine(merge2[i]);
 }
+// primary constructor
+// primary constructors were introduced to simplifying the class and struct by declaring constructor parameters with class defination.
+
+Person person = new("Hamid", 26);
+person.DisplayInfo();
+Employee employee = new("Hamid", 26, "Software Engineer");
+employee.DisplayInfo();
 
 #endregion
+
+public class Person(string name, int age)
+{
+    public void DisplayInfo()
+    {
+        Console.WriteLine($"Name is {name} and age is {age}.");
+    }
+}
+public class Employee(string name, int age, string designation) : Person(name, age)
+{
+    public string Designation { get; set; } = designation;
+}
